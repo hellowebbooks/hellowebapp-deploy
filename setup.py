58011dfcb80fafe72573e038d0e204e46ccc81ee
Tracy Osborn <tracy.j.osborn@gmail.com>
2021-04-14 07:51:43 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='hellowebapp-deploy',
-    version='1.0.3',
+    version='1.0.5',
     description="Packages needed to deploy to Heroku using Hello Web App's tutorial.",
     url='https://github.com/hellowebapp/hellowebapp-deploy',
-    download_url='https://github.com/hellowebbooks/hellowebapp-deploy/archive/v1.0.3.tar.gz',
+    download_url='https://github.com/hellowebbooks/hellowebapp-deploy/archive/v1.0.5.tar.gz',
     author='Tracy Osborn',
     author_email='tracy@limedaring.com',
     license='MIT',
@@ -26,8 +26,8 @@ setup(
     keywords='deployment heroku hellowebapp',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires = [
-        'waitress==1.1.0',
+        'waitress==2.0.0',
         'dj-database-url==0.5.0',
-        'whitenoise==3.3.1',
+        'whitenoise==5.2.0',
     ],
 )
